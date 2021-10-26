671fc8b1ea92343609f1c607b848ebde888e8a93
ShcoderTarakan <91088551+ShcoderTarakan@users.noreply.github.com>
2021-10-26 20:43:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function multiplication()
+{
+    let num1,num2,rezult;
+    num1 = document.getElementById('a').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('b').value;
+    num2 = parseInt(num2);
+    let reg = /[A-Za-zА-Яа-яЁё]/g;
+    if(reg.test(num1)||reg.test(num2) || num1<0||num2<0)
+    {
+        alert('Вводить можно только положительные числа!');
+    }
+    if(num1===0||num2===0)
+    {
+        alert('Введите значения!');
+    }
+    rezult = num1*num2;
+    if(rezult<=0)
+    {
+        alert('Ошибка! Вводите только положительные числа , которые больше 0!');
+        let str = "Ошибка!";
+        document.getElementById('out').innerHTML = str;
+    }
+    else
+    {
+        document.getElementById('out').innerHTML = rezult;
+    }
+}
+window.addEventListener('DOMContentLoaded', function (proiz) {
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("result-btn");
+    b.addEventListener("click", proiz);
+});
